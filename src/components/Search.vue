b060c3214350d0435fc7f6edc8829f891b20fe40
Mike Holzbach <maholzbach@gmail.com>
2020-08-20 09:43:50 -0400

    Add and basic router setup

---
+++ src/components/Search.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>SEARCH COMPONENT</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import Search from "./components/Search.vue";
 
 import store from "./store.js";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  base: "/",
+  mode: "history",
+  routes: [{ path: "/search", component: Search }],
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
   store,
+  router,
 }).$mount("#app");
