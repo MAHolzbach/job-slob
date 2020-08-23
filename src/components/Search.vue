f4699b941e02606cdbf909f2f02baa570e369383
Mike Holzbach <maholzbach@gmail.com>
2020-08-23 10:33:44 -0400

    Data fetch on search working

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,30 +3,42 @@
     <p class="pb-6 text-center">
       <span class="text-mainBlue">{{ totalJobNumber }}</span> jobs available
     </p>
-    <form class="shadow-lg rounded px-8 pt-6 pb-8 mb-6 border">
+    <form
+      class="shadow-lg rounded px-8 pt-6 pb-8 mb-6 border"
+      @submit="submitSearch"
+    >
       <div class="mb-4">
-        <label class="block text-darkGray text-lg mb-2 pl-1" for="what">What</label>
+        <label class="block text-darkGray text-lg mb-2 pl-1" for="what"
+          >What</label
+        >
         <input
           class="border-2 border-midGray-300 bg-white h-10 px-5 w-full rounded-lg text-md text-darkGray focus:outline-none"
-          type="search"
+          type="text"
           name="what"
           placeholder="Job title, keyword, company"
+          v-model="description"
         />
       </div>
       <div class="mb-6">
-        <label class="block text-darkGray text-lg mb-2 pl-1" for="where">Where</label>
+        <label class="block text-darkGray text-lg mb-2 pl-1" for="where"
+          >Where</label
+        >
         <input
           class="border-2 border-midGray-300 bg-white h-10 px-5 w-full rounded-lg text-md text-gray-800 focus:outline-none"
-          type="search"
+          type="text"
           name="where"
           placeholder="city, state, zip, etc."
+          v-model="location"
         />
       </div>
       <div class="flex items-center justify-between">
         <button
           class="bg-mainBlue hover:bg-blue-700 text-lightGray py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-          type="button"
-        >Search</button>
+          type="submit"
+          @click="submitSearch"
+        >
+          Search
+        </button>
       </div>
     </form>
     <Recent :recentSearches="recentSearches" />
@@ -34,14 +46,29 @@
 </template>
 
 <script>
-  import Recent from "./Recent.vue";
-  import { mapState } from "vuex";
+import Recent from "./Recent.vue";
+import { mapState } from "vuex";
 
-  export default {
-    name: "Search",
-    components: {
-      Recent,
+export default {
+  name: "Search",
+  components: {
+    Recent,
+  },
+  data() {
+    return {
+      description: "",
+      location: "",
+    };
+  },
+  computed: mapState(["totalJobNumber", "recentSearches", "searchResults"]),
+  methods: {
+    submitSearch(e) {
+      e.preventDefault();
+      this.$store.commit("searchForJobs", {
+        description: this.description,
+        location: this.location,
+      });
     },
-    computed: mapState(["totalJobNumber", "recentSearches"]),
-  };
+  },
+};
 </script>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
@@ -17,10 +18,20 @@ const store = new Vuex.Store({
       { id: 2, what: "Vue Developer", where: "Colorado", howMany: 9 },
       { id: 3, what: "Reactjs", where: "United States", howMany: 82 },
     ],
+    searchResults: [],
   },
   mutations: {
     setIsMobile(state) {
       state.isMobile = window.innerWidth < 768;
+    },
+    searchForJobs(state, payload) {
+      axios
+        .get(
+          `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${payload.description}&location=${payload.location}`
+        )
+        .then((res) => {
+          state.searchResults = res.data;
+        });
     },
   },
 });
